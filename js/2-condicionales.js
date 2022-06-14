8d517d2c4760dcf8bfc10a698c894674eb0db452
Facudiezojeda <facudiezojeda@gmail.com>
2022-06-14 19:54:34 -0300

    se agrego divisible en 2

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -1,10 +1,4 @@
 let numero1 = parseInt(prompt("Ingrese un numero"));
-let numero2 = parseInt(prompt("Ingrese otro numero"));
-let numero3 = parseInt(prompt("Ingrese un numero mas"));
-
-console.log(numero1);
-console.log(numero2);
-console.log(numero3);
 
 // if(numero1 > numero2){
 //     document.write(numero1)
@@ -15,15 +9,25 @@ console.log(numero3);
 //     }
 
 
-if(numero1 > numero2 && numero1 > numero3){
-    document.write(numero1)
-}
-else if(numero2 > numero3){
-    document.write(numero2)
-}
-else if(numero1 == numero2 && numero2 == numero3){
-    document.write("Los tres numeros son iguales.")
+// if(numero1 > numero2 && numero1 > numero3){
+//     document.write(numero1)
+// }
+// else if(numero2 > numero3){
+//     document.write(numero2)
+// }
+// else if(numero1 == numero2 && numero2 == numero3){
+//     document.write("Los tres numeros son iguales.")
+// }
+// else{
+//     document.write(numero3)
+// }
+
+
+/* divisible en 2
+if(numero1 / 2){
+document.write("Es divisible en 2")
 }
 else{
-    document.write(numero3)
-}
+    document.write("No es divisible en 2")
+}*/
+
