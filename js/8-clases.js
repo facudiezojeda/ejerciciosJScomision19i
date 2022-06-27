a4fbd4d695223905308313b09595511018070679
Facudiezojeda <facudiezojeda@gmail.com>
2022-06-27 19:36:02 -0300

    se agrego nuevo metodo y objeto a la clase alumno

--- js/8-clases.js
+++ js/8-clases.js
@@ -28,26 +28,53 @@ class Persona {
     // agregar propiedades conmutadas get y set
     //get mostrar una propiedad del objto
 
-    get mostrarNombre(){
+    get mostrarNombre() {
         return this.nombre;
     }
 
-    get mostrarApellido(){
+    get mostrarApellido() {
         return this.apellido;
     }
 
     //set modificar una propiedad del objeto
 
-    set modificarNombre (nuevoNombre){
+    set modificarNombre(nuevoNombre) {
         this.nombre = nuevoNombre;
     }
 
-    set modificarApellido (nuevoApellido){
+    set modificarApellido(nuevoApellido) {
         this.apellido = nuevoApellido;
     }
 
 }
 
+// regular o no, curso, comision, legajo, notas, cuotas al dia
+
+class Alumno extends Persona {
+    constructor(paramNombre, paramApellido, paramDNI, paramFechaNacimiento, paramEmail, paramTel, paramCurso, paramComision, paramCuotasAlDia){
+    // ejecutar constructor de la clase persona
+    super(paramNombre, paramApellido, paramDNI, paramFechaNacimiento, paramEmail, paramTel);
+    this.curso = paramCurso;
+    this.comision = paramComision;
+    this.cuotasAlDia = paramCuotasAlDia;
+
+    }
+
+    get mostrarComision(){
+        return this.comision;
+    }
+
+    mostrarDatos(){
+        document.write(`<h5>Alumno: ${this.nombre}</h5>
+        Nombre: ${this.nombre}
+        <br>Apellido: ${this.apellido}
+        <br>DNI: ${this.dni}
+        <br>Fecha de Nac: ${this.fechaNacimiento}
+        <br>Email: ${this.email} 
+        <br>Curso: ${this.curso}
+        <br>Comision: ${this.comision}`);
+    }
+}
 
 
 
@@ -61,24 +88,27 @@ let victor = new Persona("Victor", "Ignacio", 39754896, "21/04/91", "victorignac
 
 victor.mostrarDatos();
 
+let rodrigo = new Alumno("Rodrigo", "Bazan", 38998988, "12/8/95", "rb@gmail.com", 548454545, "full stack", "19i", true);
 
-let nombre = prompt("Ingrese nombre");
-let apellido = prompt("Ingrese apellido");
-let dni = prompt("Ingrese dni");
-let fechaNacimiento = prompt("Ingrese nacimiento");
-let email = prompt("Ingrese mail");
-let telefono = prompt("Ingrese telefono");
+rodrigo.mostrarDatos();
 
-let naty = new Persona(nombre, apellido, dni, fechaNacimiento, email, telefono);
 
-naty.mostrarDatos();
+// let nombre = prompt("Ingrese nombre");
+// let apellido = prompt("Ingrese apellido");
+// let dni = prompt("Ingrese dni");
+// let fechaNacimiento = prompt("Ingrese nacimiento");
+// let email = prompt("Ingrese mail");
+// let telefono = prompt("Ingrese telefono");
 
+// let naty = new Persona(nombre, apellido, dni, fechaNacimiento, email, telefono);
+
+// naty.mostrarDatos();
 //modificar propiedad de un objeto
 
-naty.modificarNombre = "Rocio";
+// naty.modificarNombre = "Rocio";
 
-naty.mostrarDatos();
+// naty.mostrarDatos();
 
 
-console.log(naty.mostrarNombre);
+//console.log(naty.mostrarNombre);
 
